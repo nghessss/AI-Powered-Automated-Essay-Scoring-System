178aa2f31cec76a822d809958c1d9bfabd64b689
Nghia Vo <voasd00@gmail.com>
2025-05-06 15:28:27 +0700

    Change port to 8080

--- backend/main.py
+++ backend/main.py
@@ -115,4 +115,5 @@ async def process_essay_endpoint(request: Feedback):
 
 # start the server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
